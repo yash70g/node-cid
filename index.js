8fb2ffe9ebfeaa58b04dbfb58e28dc2068bdab40
yash70g <yash70g@gmail.com>
2026-06-14 13:29:29 +0530

    wow2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  return res.json({'message': 'Hello World!'});
+  return res.json({'message': 'Hello World 2!'});
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
