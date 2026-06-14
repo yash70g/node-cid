2616042cd8fc9fad2b5bde5135f060953869e780
Yash Gupta <145326279+yash70g@users.noreply.github.com>
2026-06-14 14:06:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  return res.json({'message': 'Hello World 2!'});
+  return res.json({'message': 'Hello World 3!'});
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
